5a80aa8cfee53fcd7d424908dcdf9219f3686acd
Igor Souza <souza.igor@protonmail.com>
2021-09-17 11:17:53 -0300

    Update : Data annotations not needed were removed

--- Intelitrader-API/Models/UserModel.cs
+++ Intelitrader-API/Models/UserModel.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -9,23 +10,20 @@ namespace Intelitrader_API.Models
 
         [Key]
         [DataType(DataType.Text)]
-        public Guid Id { get; }
-
-        [DataType(DataType.Text)]
+        public Guid Id { get; set; }
         public string Name { get; set; }
 
         [Column(TypeName = "Date")]
         public DateTime BirthDate { get; set; }
 
-        [DataType(DataType.Text)]
         public string Sex { get; set; }
-
         public UserModel(string name, DateTime birthDate, string sex)
         {
             this.Id = Guid.NewGuid();
             this.Name = name;
-            this.BirthDate = birthDate;
             this.Sex = sex;
+            this.BirthDate = birthDate;
         }
+
     }
 }
