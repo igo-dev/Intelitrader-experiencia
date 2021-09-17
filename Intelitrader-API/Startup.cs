f53378371a3d7d4517709eba46635cb5c7097611
Igor Souza <souza.igor@protonmail.com>
2021-09-17 11:15:18 -0300

    Update : AppDataContext registered

--- Intelitrader-API/Startup.cs
+++ Intelitrader-API/Startup.cs
@@ -32,6 +32,7 @@ namespace Intelitrader_API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Intelitrader_API", Version = "v1" });
             });
+            services.AddDbContext<AppDataContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
