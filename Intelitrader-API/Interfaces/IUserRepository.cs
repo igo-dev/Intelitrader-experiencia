8af450a8b5408c4be80e98374ec6d5cdfd66f6ae
Igor Souza <souza.igor@protonmail.com>
2021-09-17 11:18:56 -0300

    Add : IUserRepository

---
+++ Intelitrader-API/Interfaces/IUserRepository.cs
@@ -0,0 +1,19 @@
+﻿using Intelitrader_API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Intelitrader_API.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<IEnumerable<UserModel>> ReadAll();
+        Task Create(UserModel userModel);
+        Task<UserModel> Read(Guid id);
+        Task Update(UserModel userModel);
+        Task Delete(Guid id);
+        Task SaveChanges();
+        Task<bool> UserExists(Guid id);
+    }
+}
