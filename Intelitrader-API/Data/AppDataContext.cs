bc4b29d3bfb7f054b1a14e2ec2dd3e5a7c3d28d6
Igor Souza <souza.igor@protonmail.com>
2021-09-17 01:17:01 -0300

    Add : AppDataContext with Users DbSet

---
+++ Intelitrader-API/Data/AppDataContext.cs
@@ -0,0 +1,28 @@
+﻿using Intelitrader_API.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Intelitrader_API.Data
+{
+    public class AppDataContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder dbContextOptionsBuilder)
+        {
+            dbContextOptionsBuilder
+                .UseSqlServer(connectionString: Environment.GetEnvironmentVariable("DB-CONNECTION"));
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserModel>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+            });
+        }
+
+        public DbSet<UserModel> Users { get; set; }
+    }
+}
