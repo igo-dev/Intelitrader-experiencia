bfa6bd353349832e32c68f1a664e5f2d0878116c
Igor Souza <souza.igor@protonmail.com>
2021-09-17 00:46:50 -0300

    Update : UserModel decorated for efcore

--- Intelitrader-API/Models/UserModel.cs
+++ Intelitrader-API/Models/UserModel.cs
@@ -1,19 +1,31 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Intelitrader_API.Models
 {
     public class UserModel
     {
+
+        [Key]
+        [DataType(DataType.Text)]
+        public Guid Id { get; }
+
+        [DataType(DataType.Text)]
+        public string Name { get; set; }
+
+        [Column(TypeName = "Date")]
+        public DateTime BirthDate { get; set; }
+
+        [DataType(DataType.Text)]
+        public string Sex { get; set; }
+
         public UserModel(string name, DateTime birthDate, string sex)
         {
             this.Id = Guid.NewGuid();
             this.Name = name;
             this.BirthDate = birthDate;
-            this.Sex = sex;   
+            this.Sex = sex;
         }
-        public Guid Id { get; }
-        public string Name { get; set; }
-        public DateTime BirthDate { get; set; }
-        public string Sex { get; set; }
     }
 }
