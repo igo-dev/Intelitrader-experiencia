303475906508f608db9e130c4fbba6d808c7d3f2
Igor Souza <souza.igor@protonmail.com>
2021-09-17 11:35:29 -0300

    Add : AutoMapperConfig

---
+++ Intelitrader-API/Data/AutoMapperConfig.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using Intelitrader_API.Dtos;
+using Intelitrader_API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Intelitrader_API.Data
+{
+    public class AutoMapperSetup : Profile
+    {
+        public AutoMapperSetup()
+        {
+            CreateMap<GetUserDto, UserModel>();
+            CreateMap<UserModel, GetUserDto>();
+
+            CreateMap<CreateUserDto, UserModel>();
+            CreateMap<UserModel, CreateUserDto>();
+
+            CreateMap<UpdateUserDto, UserModel>();
+            CreateMap<UserModel, UpdateUserDto>();
+        }
+    }
+}
