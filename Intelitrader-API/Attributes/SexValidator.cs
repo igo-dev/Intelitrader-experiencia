2325d2c6747ef800d56aeda9d4987d695cbf08a8
Igor Souza <souza.igor@protonmail.com>
2021-09-20 10:49:54 -0300

    Add : SexValidator attribute

---
+++ Intelitrader-API/Attributes/SexValidator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Intelitrader_API.Attributes
+{
+    public class SexValidator : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            string valueLower = value.ToString().ToLower();
+
+            if (valueLower == "masc" || valueLower == "fem" || valueLower == "outro")
+                return ValidationResult.Success;
+
+            return new ValidationResult("Sexo inválido. Opções válidas são 'Masc', 'Fem' e 'Outro'");
+        }
+
+    }
+}
