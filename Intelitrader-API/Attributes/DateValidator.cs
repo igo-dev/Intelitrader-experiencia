ea82cea355514476cf33139db945dec26c66d7ba
Igor Souza <souza.igor@protonmail.com>
2021-09-20 10:49:45 -0300

    Add : DateValidator attribute

---
+++ Intelitrader-API/Attributes/DateValidator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Intelitrader_API.Attributes
+{
+    public class DateValidator : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (DateTime.TryParse(value.ToString(), out var result))
+                return ValidationResult.Success;
+
+            return new ValidationResult("Data inválida. Datas devem estar no formato 'YYYY-MM-DD'");
+        }
+
+    }
+}
