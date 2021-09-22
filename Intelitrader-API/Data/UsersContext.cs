17028d978b38cbf3826021405fdf95543c1f3256
Igor Souza <souza.igor@protonmail.com>
2021-09-22 05:07:34 -0300

    Update : Connection string now comes from appsettings.json

--- Intelitrader-API/Data/UsersContext.cs
+++ Intelitrader-API/Data/UsersContext.cs
@@ -1,5 +1,6 @@
 ﻿using Intelitrader_API.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,10 +10,17 @@ namespace Intelitrader_API.Data
 {
     public class UsersContext : DbContext
     {
+        private readonly IConfiguration _configuration;
+
+        public UsersContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         protected override void OnConfiguring(DbContextOptionsBuilder dbContextOptionsBuilder)
         {
             dbContextOptionsBuilder
-                .UseSqlServer(connectionString: Environment.GetEnvironmentVariable("DB-CONNECTION"));
+                .UseSqlServer(_configuration.GetConnectionString("DefaultConnection"));
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
