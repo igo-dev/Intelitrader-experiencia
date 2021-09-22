c2a78d6aed27739d30dd3f598590e26454c33386
Igor Souza <souza.igor@protonmail.com>
2021-09-22 05:24:49 -0300

    Update : Unused namespaces removed

--- Intelitrader-API/Startup.cs
+++ Intelitrader-API/Startup.cs
@@ -1,20 +1,14 @@
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Reflection;
-using System.Threading.Tasks;
 using Intelitrader_API.Data;
 using Intelitrader_API.Interfaces;
 using Intelitrader_API.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 
 namespace Intelitrader_API
@@ -33,11 +27,12 @@ namespace Intelitrader_API
         {
             services.AddControllers().AddNewtonsoftJson();
             services.AddDbContext<UsersContext>();
+            services.AddScoped<IUserRepository, UserRepository>();
             services.AddAutoMapper(typeof(AutoMapperSetup));
 
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Intelitrader_API", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Intelitrader_API", Version = "v1" }) ;
                 var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
                 var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
                 c.IncludeXmlComments(xmlPath);
