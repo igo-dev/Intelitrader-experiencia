c4947c4a974ab87b99f71b291a5a77e375e1fc9a
Igor Souza <souza.igor@protonmail.com>
2021-10-04 19:26:57 -0300

    Update : GetUserDto's age replaced with BirthDate

--- Intelitrader-API/Dtos/GetUserDto.cs
+++ Intelitrader-API/Dtos/GetUserDto.cs
@@ -12,19 +12,14 @@ namespace Intelitrader_API.Dtos
         {
             Name = name;
             Sex = sex;
-
-            var now = DateTime.Now;
-            var age = now.Year - birthDate.Year;
-            if (birthDate > now.AddYears(-age)) age--;
-            
-            Age = age;
+            BirthDate = birthDate;
         }
         public Guid Id { get; set; }
 
         [DataType(DataType.Text)]
         public string Name { get; set; }
 
-        public int Age { get; set; }
+        public DateTime BirthDate { get; set; }
 
         [DataType(DataType.Text)]
         public string Sex { get; set; }
